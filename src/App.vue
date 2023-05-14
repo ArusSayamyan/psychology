4364e05e90d7984b3690fd112152169e19472467
Arus Sayamyan <arus.sayamyan@codetime.am>
2023-05-14 13:05:38 +0400

    add some logic and style

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,128 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="toDo">
+    <div class="toDo__container">
+      <h1 class="toDo__title">Simple To-do list</h1>
+      <div class="toDo__taskInfo">
+        <h2 class="toDo__desc">Your tasks: <span class="toDo__selectedTask">0</span> / <span class="toDo__count">{{tasksList.length}}</span></h2>
+      </div>
+      <div class="toDo__form">
+        <input type="text" class="toDo__taskDesc" v-model="taskName" @keyup.enter="addTask">
+        <button class="toDo__addBtn" @click="addTask">Add</button>
+      </div>
+      <div class="toDo__taskList">
+        <ul class="toDo__taskWrapper" v-for="task in tasksList" :key="task">
+          <li class="toDo__taskName" >
+            <div class="toDo__taskContent">
+              <input type="checkbox" class="toDo__check">
+              <span class="toDo__taskText">{{ task }}</span>
+            </div>
+            <button class="toDo__remove">Remove</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    const taskName = ref();
+    const addedTask = ref(false);
+    const tasksList = ref([])
+
+    function addTask() {
+      if(taskName.value) {
+        tasksList.value.unshift(taskName.value);
+        taskName.value = '';
+      }
+    }
+
+    return {
+      taskName,
+      addedTask,
+      addTask,
+      tasksList,
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+  margin: 0;
+}
+.toDo {
+  max-width: 700px;
+  width: 100%;
+  background: #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  padding: 20px;
+
+  &__container {
+    width: 100%;
+  }
+
+  &__taskDesc {
+    background: #fff;
+    border: 1px solid #000;
+    border-radius: 8px;
+    padding: 10px 15px;
+    max-width: 200px;
+    width: 100%;
+    margin-right: 15px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  &__addBtn {
+    padding: 10px;
+    border: none;
+    background: darkgray;
+    font-weight: 700;
+    color: blue;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+
+  &__taskName {
+    list-style: none;
+    padding: 15px;
+    margin: 0;
+    background: lightsteelblue;
+    border-radius: 8px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__taskWrapper {
+    padding: 0;
+  }
+
+  &__remove {
+    padding: 8px;
+    border: none;
+    background: red;
+    border-radius: 8px;
+    color: #fff;
+    font-weight: 700;
+
+    &:hover {
+      background: #fff;
+      outline: 1px solid red;
+      color: red;
+      cursor: pointer;
+    }
+  }
 }
 </style>
